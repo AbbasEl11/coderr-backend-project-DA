36192ea5ce4c884efe64b688cd54585845279f65
Abbas El <abbas-el-mahmoud@hotmail.com>
2025-12-09 14:36:47 +0100

    feat(order): update order queryset logic for user filtering

--- order_app/api/views.py
+++ order_app/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import AllowAny
-from django.db.models import Min
+from django.db.models import Q
 from rest_framework import viewsets
 from order_app.api.serializers import OrderSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -11,4 +11,15 @@ from order_app.models import Order
 class OrderViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+    queryset = None
+
+    def get_queryset(self):
+        action = self.action
+
+        if action == 'list':
+            return Order.objects.filter(
+                Q(customer_user=self.request.user) | Q(
+                    business_user=self.request.user)
+            )
+        else:
+            return Order.objects.all()
